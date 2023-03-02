2fff3af07240dc4d38fe06b2840be7ae9fefbbb8
Bex G <rkmoody1@gmail.com>
2023-03-02 12:10:04 -0700

    Create a game function that compares player and computer selected values.

--- script.js
+++ script.js
@@ -14,22 +14,24 @@ if (choice === 0) {
     computerSelection = arr[2];
   }
 
-let playerSelection = prompt("So what'll it be? Rock, paper or scissors?")
 
-function game(playerSelection, computerSelection) {
 
-    if (playerSelection === computerSelection) {
-        alert("It's a tie! Try again.");
-    } else if (playerSelection === arr[1] && computerSelection === arr[2]) {
-        alert("Paper beats rock. You Win!");
-    } else if (playerSelection === arr[1] && computerSelection === arr[3]) {
-        alert("Rock beats scissors. You Win!");
-    } else if (playerSelection === arr[2] && computerSelection === arr[1]) {
-        alert("Rock beats paper. You lose!");
-    } else if (playerSelection === arr[2] && computerSelection === arr[3]) {
-        alert("Scissors beat paper. You lose!");
-    } else if (playerSelection === arr[3] && computerSelection === arr[1]) {
-        alert("Rock beats scissors. You lose!");
-    } else if (playerSelection === arr[3] && computerSelection === arr[2]) 
-        alert("Scissors beat paper. You win!");
-    }
+  
+
+  function game(playerSelection, computerSelection) {
+  
+      if (playerSelection === computerSelection) {
+          alert("It's a tie! Try again.");
+      } else if (playerSelection === arr[1] && computerSelection === arr[2]) {
+          alert("Paper beats rock. You Win!");
+      } else if (playerSelection === arr[1] && computerSelection === arr[3]) {
+          alert("Rock beats scissors. You Win!");
+      } else if (playerSelection === arr[2] && computerSelection === arr[1]) {
+          alert("Rock beats paper. You lose!");
+      } else if (playerSelection === arr[2] && computerSelection === arr[3]) {
+          alert("Scissors beat paper. You lose!");
+      } else if (playerSelection === arr[3] && computerSelection === arr[1]) {
+          alert("Rock beats scissors. You lose!");
+      } else if (playerSelection === arr[3] && computerSelection === arr[2]) 
+          alert("Scissors beat paper. You win!");
+      }
