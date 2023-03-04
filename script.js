9daa6dfafce29d1b9d1e71c272d4fce1e4c78053
Bex G <rkmoody1@gmail.com>
2023-03-03 18:17:35 -0700

    Add for loop to make game replayable.

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 // For computerSelection: 0 = "rock," 1 = "paper," 2 = "scissors." //
 
+let playerSelection = prompt("So what'll it be? Rock, paper or scissors?").toLowerCase();
 
-playerSelection = prompt("So what'll it be? Rock, paper or scissors?").toLowerCase();
-
-computerSelection = Math.floor(Math.random() * 3);
+let computerSelection = Math.floor(Math.random() * 3);
 
 function playRound(playerSelection, computerSelection) {
   
     if (playerSelection === "rock" && computerSelection === 1) {
-        return("Paper beats rock. You lose!");
+        return("Paper beats rock. You lose.");
     } else if (playerSelection === "rock" && computerSelection === 2) {
         return("Rock beats scissors. You win!");
     } else if (playerSelection === "paper" && computerSelection === 0) {
-        return("Rock beats paper. You lose!");
+        return("Rock beats paper. You lose.");
     } else if (playerSelection === "paper" && computerSelection === 2) {
-        return("Scissors beat paper. You lose!");
+        return("Scissors beat paper. You lose.");
     } else if (playerSelection === "scissors" && computerSelection === 0) {
-        return("Rock beats scissors. You lose!");
+        return("Rock beats scissors. You lose.");
     } else if (playerSelection === "scissors" && computerSelection === 1) {
         return("Scissors beat paper. You win!");
     } else return("It's a tie! Try again.");
+}
+
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+
+        playerSelection = prompt("So what'll it be? Rock, paper or scissors?").toLowerCase();
+
+        computerSelection = Math.floor(Math.random() * 3);
+
+        return playRound(playerSelection, computerSelection);
+    }
 }
