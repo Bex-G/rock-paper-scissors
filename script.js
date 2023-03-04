bcd63ff87a89468f4467ddfa2146af76b0e098c5
Bex G <rkmoody1@gmail.com>
2023-03-03 17:29:34 -0700

    Simplify code by removing array values.

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
-let arr = ["rock", "paper", "scissors"];
+// For computerSelection: 0 = "rock," 1 = "paper," 2 = "scissors." //
 
-// Generate random number. //
 
-getComputerChoice = Math.floor(Math.random() * arr.length);
+playerSelection = prompt("So what'll it be? Rock, paper or scissors?").toLowerCase();
 
-// Assign random number to array value. //
-
-let computerchoice = getComputerChoice;
-
-if (computerchoice === 0) {
-    computerSelection = arr[0];
-  } else if (computerchoice === 1) {
-    computerSelection = arr[1];
-  } else if (computerchoice === 2) {
-    computerSelection = arr[2];
-  }
+computerSelection = Math.floor(Math.random() * 3);
 
 function playRound(playerSelection, computerSelection) {
   
-    if (playerSelection === computerSelection) {
-        return("It's a tie! Try again.");
-    } else if (playerSelection === "rock" && computerSelection === arr[1]) {
+    if (playerSelection === "rock" && computerSelection === 1) {
         return("Paper beats rock. You lose!");
-    } else if (playerSelection === "rock" && computerSelection === arr[2]) {
+    } else if (playerSelection === "rock" && computerSelection === 2) {
         return("Rock beats scissors. You win!");
-    } else if (playerSelection === "paper" && computerSelection === arr[0]) {
+    } else if (playerSelection === "paper" && computerSelection === 0) {
         return("Rock beats paper. You lose!");
-    } else if (playerSelection === "paper" && computerSelection === arr[2]) {
+    } else if (playerSelection === "paper" && computerSelection === 2) {
         return("Scissors beat paper. You lose!");
-    } else if (playerSelection === "scissors" && computerSelection === arr[0]) {
+    } else if (playerSelection === "scissors" && computerSelection === 0) {
         return("Rock beats scissors. You lose!");
-    } else if (playerSelection === "scissors" && computerSelection === arr[1]) 
+    } else if (playerSelection === "scissors" && computerSelection === 1) {
         return("Scissors beat paper. You win!");
-    }
-
-    
-    
-    // Prompt player for input. //
-
-let playerSelection = prompt("So what'll it be? Rock, paper or scissors?").toLowerCase();
-
+    } else return("It's a tie! Try again.");
+}
