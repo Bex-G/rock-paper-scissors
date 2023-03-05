423ab8609d457f0d7aa9d9a280d8bd12e234392a
Bex G <rkmoody1@gmail.com>
2023-03-05 16:44:18 -0700

    Move score-keeping log to fix game loop.

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
 // For computerSelection: 0 = "rock," 1 = "paper," 2 = "scissors." //
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   
     if (playerSelection === "rock" && computerSelection === 1) {
+        console.log ("score: " + playerScore + " | " + ++computerScore);
         return ("Paper beats rock. You lose.");
     } else if (playerSelection === "rock" && computerSelection === 2) {
+        console.log ("score: " + ++playerScore + " | " + computerScore);
         return ("Rock beats scissors. You win!");
     } else if (playerSelection === "paper" && computerSelection === 0) {
-        return ("Rock beats paper. You lose.");
+        console.log ("score: " + ++playerScore + " | " + computerScore);
+        return ("Paper beats rock. You win!");
     } else if (playerSelection === "paper" && computerSelection === 2) {
+        console.log ("score: " + playerScore + " | " + ++computerScore);
         return ("Scissors beat paper. You lose.");
     } else if (playerSelection === "scissors" && computerSelection === 0) {
+        console.log ("score: " + playerScore + " | " + ++computerScore);
         return ("Rock beats scissors. You lose.");
     } else if (playerSelection === "scissors" && computerSelection === 1) {
+        console.log ("score: " + ++playerScore + " | " + computerScore);
         return ("Scissors beat paper. You win!");
-    } else return("It's a tie! Try again?");
-}
-
-function score() {
-    
-    let playerScore = 0;
-    let computerScore = 0;
-
-    if (playRound(playerSelection, computerSelection).endsWith("!")) {
-        return "score: " + ++playerScore + " | " + computerScore;
-    } else if (playRound(playerSelection, computerSelection).endsWith(".")) {
-        return "score: " + playerScore + " | " + ++computerScore;
-    } else if (playRound(playerSelection, computerSelection).endsWith("?")) {
-        return "score: " + playerScore + " | " + computerScore;
-    }
-
-    if (i = 5 && playerScore > computerScore) {
-        console.log("Congrats, you won the game!")
-    } else if (i = 5 && playerScore < computerScore) {
-        console.log("Better luck next time.")
-    } else if (i = 5 && playerScore === computerScore) {
-        console.log("It's a tie! Good game. :)")
-    }
+    } else console.log ("score: " + +playerScore + " | " + computerScore);
+        return ("It's a tie! Try again?");
 }
 
 function game() {
@@ -47,6 +35,6 @@ function game() {
 
         computerSelection = Math.floor(Math.random() * 3);
 
-        console.log(playRound(playerSelection, computerSelection), score());
-    }
+        console.log(playRound(playerSelection, computerSelection));
+    } 
 }
