2584a81ffa1990ad4c954f168a563d544e543c3d
Bex G <rkmoody1@gmail.com>
2023-03-05 21:06:34 -0700

    Create a function that calculates the final score and set it to announce a winner after 5 rounds of the game.

--- script.js
+++ script.js
@@ -23,8 +23,19 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "scissors" && computerSelection === 1) {
         console.log ("score: " + ++playerScore + " | " + computerScore);
         return ("Scissors beat paper. You win!");
-    } else console.log ("score: " + +playerScore + " | " + computerScore);
+    } else console.log ("score: " + playerScore + " | " + computerScore);
         return ("It's a tie! Try again?");
+}
+
+function finalScore () {
+    
+    if (playerScore > computerScore) {
+        console.log("Congrats, you won the game!")
+    } else if (playerScore < computerScore) {
+        console.log("Better luck next time.")
+    } else if (playerScore = computerScore) {
+        console.log("It's a tie! Good game. :)")
+    }
 }
 
 function game() {
@@ -37,4 +48,5 @@ function game() {
 
         console.log(playRound(playerSelection, computerSelection));
     } 
+    console.log(finalScore());
 }
