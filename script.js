5fa30f185e337ce66940cba0666fa18fb2a8f243
Bex-G <bexgiacinto@gmail.com>
2026-01-29 16:46:22 -0800

    Rewrote program with bug fixes and a slightly differing UI.

--- script.js
+++ script.js
@@ -1,92 +1,89 @@
+const playerDisplay = document.querySelector("#playerDisplay");
+const cpDisplay = document.querySelector("#cpDisplay");
+const roundDescription = document.querySelector("#roundDescription");
+const roundWinner = document.querySelector("#roundWinner");
 let playerScore = 0;
-let computerScore = 0;
+let cpScore = 0;
 
-function getComputerSelection() {
+// each string value has a corresponding number --> rock = 0, paper = 1, scissors = 2
 
-    let randomNumber = Math.floor(Math.random() * 3);
-
-    if (randomNumber === 0) {
-    computerSelection = "rock";
-    } else if (randomNumber === 1) {
-    computerSelection = "paper";
-    } else if (randomNumber === 2) {
-    computerSelection = "scissors";
-    }
-}
-
-//Plays one round and returns score for each button click event.
-
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', () => {
-    getComputerSelection();
-    playRound("rock", computerSelection);
-    gamePlay("Rock");
-    scoreKeeper();
-    finalScore();
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+  playerNum = 0;
+  playerString = "rock";
+  runRound(playerNum);
 });
 
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', () => {
-    getComputerSelection();
-    playRound("paper", computerSelection);
-    gamePlay("Paper");
-    scoreKeeper();
-    finalScore();
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+  playerNum = 1;
+  playerString = "paper";
+  runRound(playerNum);
 });
 
-const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', () => {
-    getComputerSelection();
-    playRound("scissors", computerSelection);
-    gamePlay("Scissors");
-    scoreKeeper();
-    finalScore();
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+  playerNum = 2;
+  playerString = "scissors";
+  runRound(playerNum);
 });
 
-
-function playRound(playerSelection, computerSelection) {
-  
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore += 1;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore += 1;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore += 1;
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore += 1;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore += 1;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore += 1;
-    }
+function runRound(playerNum) {
+  cpNum = Math.floor(Math.random() * 3); // get random number for comparison
+  getCpString(cpNum); // assign a string value to the corresponding random number
+  getScore(playerNum, cpNum, playerString, cpString); // compare numbers to determine and display strings
+  roundDescription.textContent =
+    "You chose " + playerString + ", and your opponent chose " + cpString + "."; // dynamically display cp and player selections
 }
 
-function gamePlay(playerSelection) { //Records selections as readable text.
-
-    const game = document.querySelector('#game');
-    
-    const gameContent = document.querySelector('#gameP');
-    gameContent.classList.add('content');
-    gameContent.textContent = playerSelection + " vs " + computerSelection + "!"
-    
-    game.appendChild(gameContent);
+function getCpString(cpNum) {
+  if (cpNum === 0) {
+    return (cpString = "rock");
+  } else if (cpNum === 1) {
+    return (cpString = "paper");
+  } else if (cpNum === 2) {
+    return (cpString = "scissors");
+  }
 }
 
-function scoreKeeper() { //Displays score totals.
-
-    const score = document.querySelector('#score');
-    
-    const scoreContent = document.querySelector('h3');
-    scoreContent.classList.add('content');
-    scoreContent.textContent = "Player: " + playerScore + " | Computer: " + computerScore;
-    
-    score.appendChild(scoreContent);
+function getScore(playerNum, cpNum, playerString, cpString) {
+  if (playerNum === cpNum) {
+    roundWinner.textContent = "It's a tie!";
+  } else if (playerNum === 0 && cpNum === 2) {
+    playerWins(playerString, cpString);
+  } else if (playerNum === 2 && cpNum === 0) {
+    cpWins(playerString, cpString);
+  } else if (playerNum > cpNum) {
+    playerWins(playerString, cpString);
+  } else {
+    cpWins(playerString, cpString);
+  }
+  checkForWinner(playerScore, cpScore);
 }
 
-function finalScore() {
-    if (playerScore === 5) {
-        alert("Congrats, you won the game!")
-    } else if (computerScore === 5) {
-        alert("Better luck next time.")
-    }
+function cpWins(playerString, cpString) {
+  ++cpScore;
+  cpDisplay.textContent = "Computer score: " + cpScore;
+  newString =
+    cpString + " beats " + playerString + ". Computer wins this round.";
+  roundWinner.textContent =
+    newString.charAt(0).toUpperCase() + newString.slice(1);
+  return cpScore;
+}
+
+function playerWins(playerString, cpString) {
+  ++playerScore;
+  playerDisplay.textContent = "Player score: " + playerScore;
+  newString = playerString + " beats " + cpString + ". You win this round!";
+  roundWinner.textContent =
+    newString.charAt(0).toUpperCase() + newString.slice(1);
+  return playerScore;
+}
+
+function checkForWinner(playerScore, cpScore) {
+  if (playerScore >= 3) {
+    alert("You're a winner, baby!");
+  } else if (cpScore >= 3) {
+    alert("Better luck next time.");
+  }
 }
